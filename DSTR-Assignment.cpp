62ba71f71eeb5b8c28fd5077a9e2ea4bdc644da4
Khor Zhen Win <khorzhenwin@gmail.com>
2022-04-05 19:13:15 +0800

    Cleanup namespace + tested including the header file

--- DSTR-Assignment.cpp
+++ DSTR-Assignment.cpp
@@ -1,13 +1,14 @@
-
 #include "pch.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    // create user object
+    User *user1 = new User(1, "user1", "password1", 0);
+    // cout user object
+    cout << user1->userId << " " << user1->username << " " << user1->password << " " << user1->userType << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
-
