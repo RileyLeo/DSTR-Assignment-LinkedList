4f6f1791c9725618c0cfb4099569b79bc199e899
Khor Zhen Win <khorzhenwin@gmail.com>
2022-04-05 19:07:16 +0800

    Created a sample user class hopefull it works

---
+++ user.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "includes.h"
+
+// create namespace for this header file
+namespace user
+{
+    // create user class doubly linked list with unsignedInt: userId, string: username, string: password, unsignedInt: userType
+    class User
+    {
+    public:
+        unsigned int userId;
+        std::string username;
+        std::string password;
+        unsigned int userType; // 0-HR, 1-Admin, 2- Tutor
+        User *nextAddress;
+        User *previousAddress;
+
+        // constructor
+        User(unsigned int userId, std::string username, std::string password, unsigned int userType)
+        {
+            this->userId = userId;
+            this->username = username;
+            this->password = password;
+            this->userType = userType;
+            this->nextAddress = nullptr;
+            this->previousAddress = nullptr;
+        }
+    } *head = NULL;
+
+}
